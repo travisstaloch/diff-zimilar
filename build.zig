0206943b62e91a20a9581edd318ebe888049485f
Travis Staloch <twostepted@gmail.com>
2023-02-16 11:48:02 -0800

    disable run_cmd caching
    fix for suppressed 2nd run of `zig build run -- ..`

--- build.zig
+++ build.zig
@@ -31,9 +31,9 @@ pub fn build(b: *std.Build) void {
     exe.install();
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    run_cmd.condition = .always;
+    if (b.args) |args| run_cmd.addArgs(args);
+
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
